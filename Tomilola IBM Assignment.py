48e72344ab566d12a3e6a1e744b163e7c22d5842
TomilolaMak <114153760+TomilolaMak@users.noreply.github.com>
2022-11-22 11:58:24 +0100

    Add files via upload

---
+++ Tomilola IBM Assignment.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Tomilola Makanju__
+# 
+# Desired Occupation - Data Scientist
+
+# _I am interested in data science because I want to apply analytics driven decisions to solve core issues in Animal Science. I also want to get a better paying job_
+
+# ### This is a simple addition code
+
+# In[1]:
+
+
+a = 23
+b = 12
+sumofAandB = a+b
+print(sumofAandB)
+
+
+# Bulleted list
+# - Shoes
+#     Flats
+#     Court shoes
+# - Backpack
+# - Toiletries
+# 
+# Numbered List
+# 1. Shoes
+# 2. Backpack
+# 3. Toiletries
+# 
+# Image![Data%20image.jpg](attachment:Data%20image.jpg)
+
+# In[ ]:
+
+
+
+
